d32459b8923a4fd134468fc9ea3ed460ede63553
Chenna Reddy <51876148+chennareddyb@users.noreply.github.com>
2025-02-18 05:09:20 -0400

    Create pythonpart6_20feb25_db.py

---
+++ pythonpart6_20feb25_db.py
@@ -0,0 +1,41 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/chennareddyb/python_training/blob/Feb2025/pythonpart5_18feb25_oops.ipynb\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "markdown",
+      "source": [
+        "**class**\\\n",
+        "class is a combination of related data\n",
+        "\n",
+        "object is the instance of the class"
+      ],
+      "metadata": {
+        "id": "Y74jOyNWoMWn"
+      }
+    }
+  ]
+}
